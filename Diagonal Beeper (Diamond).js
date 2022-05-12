01c717d058c32647d17cc40fd45075cdaaad15d9
FirstThinker_Swapnil <92979885+Swap-Nova@users.noreply.github.com>
2022-05-12 16:00:03 +0530

    Update Diagonal Beeper (Diamond).js

--- Diagonal Beeper (Diamond).js
+++ Diagonal Beeper (Diamond).js
@@ -1,1 +1,36 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+   putBeeper();
+   move();
+   
+   turning_Left();
+   
+   putBeeper();
+   move();
+   turning_Right();
+   
+   putBeeper();
+   
+   turning_Left();
+   turning_Right();
+   
+   putBeeper();
+   
+   turning_Left();
+   turning_Right();
+   putBeeper();
+}
 
+function turning_Left(){
+   turnLeft();
+   move();
+}
+
+function turning_Right(){
+   turnRight();
+   move();
+}
