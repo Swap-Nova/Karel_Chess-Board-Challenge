f836adc445f29c52c28f206d8715fa9aefd57e08
FirstThinker_Swapnil <92979885+Swap-Nova@users.noreply.github.com>
2022-05-12 15:21:16 +0530

    Create Moving to Top Right.js

---
+++ Moving to Top Right.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+   move();
+   move();
+   
+   turnLeft();
+   moveSixTimes();
+   
+   turnRight();
+   moveFourTimes();
+}
+
+function moveFourTimes(){
+   move();
+   move();
+   move();
+   move();
+}
+
+function moveSixTimes(){
+   move();
+   move();
+   move();
+   move();
+   move();
+   move();
+}
